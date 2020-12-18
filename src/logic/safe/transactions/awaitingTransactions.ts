526ae68603ee2e36862fe483702c3261c65a8646
fernandomg <fernando.greco@gmail.com>
2020-12-18 15:14:42 -0300

    missing types

--- src/logic/safe/transactions/awaitingTransactions.ts
+++ src/logic/safe/transactions/awaitingTransactions.ts
@@ -5,14 +5,14 @@ import { Transaction } from 'src/logic/safe/store/models/types/transaction'
 
 export const getAwaitingTransactions = (
   allTransactions: List<Transaction>,
-  cancellationTxs,
+  cancellationTxs: Map<string, Transaction>,
   userAccount: string,
 ): List<Transaction> => {
   return allTransactions.filter((tx) => {
     const cancelTx = !!tx.nonce && !isNaN(Number(tx.nonce)) ? cancellationTxs.get(`${tx.nonce}`) : null
 
     // The transaction is not executed and is not cancelled, nor pending, so it's still waiting confirmations
-    if (!tx.executionTxHash && !tx.cancelled && !isPendingTransaction(tx, cancelTx)) {
+    if (!tx.executionTxHash && !tx.cancelled && cancelTx && !isPendingTransaction(tx, cancelTx)) {
       // Then we check if the waiting confirmations are not from the current user, otherwise, filters this transaction
       const transactionWaitingUser = tx.confirmations.filter(({ owner }) => owner !== userAccount)
       return transactionWaitingUser.size > 0
